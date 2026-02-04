c56e8b9648f28f41051cbc3f749c1a513f2231a5
Tej Tilekar <ttileka@calstatela.edu>
2026-02-03 22:15:18 -0800

    ui: add global nav bar layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,13 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Navbar from "@/components/navbar";
+
+<body>
+  <Navbar />
+</body>
+ 
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,8 +34,10 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <Navbar />
         {children}
       </body>
     </html>
   );
 }
+
