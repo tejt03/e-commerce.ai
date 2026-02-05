cf90bd4cefae6059ca5e7ea3b47d3a3d17faa1b6
Tej Tilekar <ttileka@calstatela.edu>
2026-02-05 14:26:33 -0800

    fix: force dyamic rendering for admin auth

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,3 +1,5 @@
+export const dynamic = "force-dynamic";
+
 import { redirect } from "next/navigation";
 import { supabaseServer } from "@/lib/supabase-server";
 import AdminClient from "@/components/AdminClient";
